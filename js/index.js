9818cb0d41461859539ef182cbd6738ee4bfe360
Nusrat Jahan <140106780+nusrat40@users.noreply.github.com>
2024-09-24 21:02:49 +0600

    added the blog section

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-blog").addEventListener("click",function(){
+    
+    window.location.href="/blog.html";
+});
+
+document.getElementById("btn-donate").addEventListener("click",function(event){
+    
+})
---
+++ js/utilities.js
@@ -0,0 +1,22 @@
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber=parseFloat(inputValue);
+    return inputNumber;
+}
+
+function getTextFieldValuById(id){
+  const textValue=document.getElementById(id).innerText;
+  const textNumber=parseFloat(textValue);
+  return textNumber;
+}
+
+function showSectionById(id){
+    document.getElementById("add-money-form").classList.add("hidden");
+    document.getElementById("cash-out-form").classList.add("hidden");
+    document.getElementById("transaction-form").classList.add("hidden");
+
+    //show the form which is clicked
+    document.getElementById(id).classList.remove("hidden");
+
+}
+
